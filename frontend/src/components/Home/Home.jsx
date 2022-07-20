2e464d3fc9f10c96e7eb1f72616cf60fa8cf1881
ShreyanshGuptaWD <shreyanshgupta148@gmail.com>
2022-07-20 20:54:29 +0530

    update

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 
+import Carousel from "./Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 function HomePage(){
     return(
         <>
+          <Carousel/>
+      
  <h1>Home</h1>
         </>
     )
