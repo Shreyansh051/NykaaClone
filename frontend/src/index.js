a7ac4da048cf2a12e2291889a62a2fcfe566612b
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-20 17:50:27 +0530

    npm packages update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import {legacy_createStore, applyMiddleware, compose} from "redux";
+import thunk from 'redux-thunk';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = legacy_createStore()
 root.render(
   <BrowserRouter>
     <App />
