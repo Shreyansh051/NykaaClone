c869e7e69a33b22ce1d1a82a3611c23c36e6f793
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-20 14:32:47 +0530

    2nd commit

---
+++ backend/Controllers/productcontroller.js
@@ -0,0 +1,10 @@
+const Products = require("../Models/ProductModel");
+
+module.exports.getProducts = async (req, res) => {
+  try {
+    const getAllProducts = await Products.find();
+    res.status(200).json({ result: getAllProducts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
--- backend/storage/db.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports= mongoose.connect("mongodb://localhost:27017/Nykaa")
