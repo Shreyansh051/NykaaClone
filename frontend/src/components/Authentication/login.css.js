09856436ecf00c194d7a9a7e8e11480254868f91
SatsAjeet <98746450+SatsAjeet@users.noreply.github.com>
2022-07-20 14:42:23 +0530

    .

--- frontend/src/components/Authentication/login.css.js
+++ frontend/src/components/Authentication/login.css.js
@@ -1,6 +1,9 @@
 const container = {
     boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
-    width: "30%",
+    width: "27%",
+    height: "900px",
+    margin: "100px auto",
+    borderRadius: "15px",
 }
 
 export {container}
