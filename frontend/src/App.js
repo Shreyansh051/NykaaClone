a6dc45a4353ac4f88b97b02c622fad7e01729562
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-21 11:34:50 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import HomePages from "./components/Home/HomePage"
+import Products from "./components/Product page/Products";
 function App() {
   return (
-    <HomePages/>
+    // <HomePages/>
+    <Products/>
   );
 }
 
