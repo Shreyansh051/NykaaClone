a5a73fc36e27a73a307947dd81393a20b131d9e0
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-21 11:25:44 +0530

    update

--- frontend/src/components/Product page/Products.jsx
+++ frontend/src/components/Product page/Products.jsx
@@ -7,6 +7,7 @@ import styles from "./Products.module.css";
 
 const Products = () => {
   const { products } = useSelector((state) => state);
+  console.log(products);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllProducts());
