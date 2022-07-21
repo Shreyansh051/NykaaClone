f1911d8ca10d39d48bcf0fb68ec9e83df816955f
SatsAjeet <98746450+SatsAjeet@users.noreply.github.com>
2022-07-21 16:00:52 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/", productRoute);
 //<---------------------------------------------------------------->
 const CONNECTION_URL = 'mongodb+srv://hlv-kakashi:Anupam123@cluster0.tjkwwu3.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 8080;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 // import HomePages from "./components/Home/HomePage"
 import Products from "./components/Product page/Products";
+import Main from './Main';
 function App() {
   return (
     <div className="App">
-    <Products/>
+      {/* <Products/> */}
+      <Main/>
     </div>
   );
 }
