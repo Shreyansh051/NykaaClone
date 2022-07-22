827e942f0ada4a95aba68c18ec1eb93771d50892
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-22 14:28:08 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './Main';
 import Products from './components/Product page/Products';
+// import { Siderbar } from './components/Product page/Sidebar';
 function App() {
   return (
     <div className="App">
     <Products/>
+    {/* <Siderbar/> */}
     </div>
   );
 }
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,3 +3,7 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const fetch_AllProducts = () => API.get("/getAllProducts");
+export const Add_to_cart = (id) => API.post(`/AddToBag/${id}`)
+export const get_all_cart = () => API.get("/getAllCart");
+export const set_quantity = (id,quant) => API.patch(`/setQuantity/${id}`,{ quant});
+export const delete_item = (id) => API.delete(`/cart/${id}`);
