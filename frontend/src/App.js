f5fd9f4446da5d6e62768422fc8b4b7dba611c9a
ShreyanshGuptaWD <shreyanshgupta148@gmail.com>
2022-07-22 23:07:18 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 
 import HomePages from "./components/Home/HomePage"
 // import Products from "./components/Product page/Products";
-import Main from './Main';
-import Products from './components/Product page/Products';
+// import Main from './Main';
+// import Products from './components/Product page/Products';
 // import { Siderbar } from './components/Product page/Sidebar';
 function App() {
   return (
     <div className="App">
-    <Products/>
+    {/* <Products/> */}
+    <HomePages/>
     {/* <Siderbar/> */}
     </div>
   )};
