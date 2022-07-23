36451b0a65b2e28893952081fac7de87c427b797
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-23 18:20:18 +0530

    update

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,5 +1,5 @@
 const UserModel = require("../Models/UserModel")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const SECRET = "NYKAACLONESECRET"
 //<------------------------------------------------------------------------------------------------>
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,14 @@ import Products from './components/Product page/Products';
 
 import HomePage from "./components/Home/HomePage"
 // import Products from "./components/Product page/Products";
-// import Main from './Main';
+import Main from './Main';
 // import Products from './components/Product page/Products';
 // import { Siderbar } from './components/Product page/Sidebar';
 function App() {
   return (
     <div className="App">
     {/* <Products/> */}
+    {/* <Main/> */}
     <HomePage/>
     {/* <Siderbar/> */}
     </div>
