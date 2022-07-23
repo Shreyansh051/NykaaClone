157a4158f7313cc839b0115b4ff5fd746b9af793
ShreyanshGuptaWD <shreyanshgupta148@gmail.com>
2022-07-23 14:35:19 +0530

    update

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -15,7 +15,7 @@ import {
 
 const cardImages1 = [
   {
-    url: "https://images-static.nykaa.com/uploads/046de71f-4797-44bc-b7d2-1c198e65d050.png?tr=w-1200,cm-pad_resize",    
+    url: "https://images-static.nykaa.com/uploads/046de71f-4797-44bc-b7d2-1c198e65d050.png?tr=w-1200,cm-pad_resize",
   },
   {
     url: "https://images-static.nykaa.com/uploads/57c0cdd9-3cbc-470e-a47b-c08ee7ca0144.png?tr=w-1200,cm-pad_resize",
@@ -25,97 +25,94 @@ const cardImages1 = [
 const cardImages2 = [
   {
     url: "https://images-static.nykaa.com/uploads/45e77657-2fba-49f1-b077-437dc02b978c.gif?tr=w-1200,cm-pad_resize",
-    tittle:"For Every Skin Type",
-    discount:" Upto 35% Off%"
+    tittle: "For Every Skin Type",
+    discount: " Upto 35% Off%",
   },
   {
     url: "https://images-static.nykaa.com/uploads/2aaabbf7-64bb-4ec8-b73c-671074554789.jpg?tr=w-1200,cm-pad_resize",
-    tittle:"On Entire Range",
-    discount:" Upto 25% Off"
-   
-
+    tittle: "On Entire Range",
+    discount: " Upto 25% Off",
   },
   {
     url: "https://images-static.nykaa.com/uploads/ebbf5f5b-9f8c-4839-9063-d4b3d4fb5656.gif?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "For Every Skin Type",
+    discount: "Upto 35% Off%",
   },
   {
     url: "https://images-static.nykaa.com/uploads/1555b61a-a163-408a-ab3d-3a666832acfb.jpg?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: " Explore Waterproof Makeup +",
+    discount: "Exciting Gifts On Orders!",
   },
+
   {
     url: "https://images-static.nykaa.com/uploads/83247895-0a7e-4141-9b86-ce7b039e7c3a.gif?tr=w-1200,cm-pad_resize",
-    tittle:"For Every Skin Type",
-    discount:"Upto 35% Off"
+    tittle: "For Every Skin Type",
+    discount: "Exciting Gifts On Orders!",
   },
   {
     url: "https://images-static.nykaa.com/uploads/3856736f-82d6-45c0-a10e-8bc90e891bb2.gif?tr=w-1200,cm-pad_resize",
-    tittle:"On Entire Range",
-    discount:"Upto 25% Off"
+    tittle: "On Entire Range",
+    discount: "Upto 25% Off",
   },
   {
     url: "https://images-static.nykaa.com/uploads/b1189a45-6537-415f-ae55-bdd500d074b0.gif?tr=w-1200,cm-pad_resize",
-    tittle:" worth Rs. 525 on Rs. 800",
-    discount:"Upto 40% Off + Free lipstick"
+    tittle: " worth Rs. 525 on Rs. 800",
+    discount: "Upto 40% Off + Free lipstick",
   },
   {
     url: "https://images-static.nykaa.com/uploads/64c65727-2d07-41a5-b4a5-6e0dbfee8b98.jpg?tr=w-1200,cm-pad_resize",
-    tittle:"SKY HIGH Mascara Has Landed In India !",
-    discount:"Upto 30% Off On Maybelline Bestsellers  "
-    
-
+    tittle: "SKY HIGH Mascara Has Landed In India !",
+    discount: "Upto 30% Off On Maybelline Bestsellers  ",
   },
 ];
 const cardImages3 = [
   {
     url: "https://images-static.nykaa.com/uploads/db316f35-4e1f-49a2-942e-c730599ff776.gif?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "Moisturizer Worth Rs. 399 On Rs. 1000",
+    discount: "Upto 15% Off + Free 5% Niacinamide",
   },
   {
     url: "https://images-static.nykaa.com/uploads/4e85e306-4fcf-463a-ae04-d0066fb010de.gif?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "explore more+",
+    discount: "Upto 20% Off",
   },
 ];
 const cardImages31 = [
   {
     url: "https://images-static.nykaa.com/uploads/4c447fa5-0de0-4c69-a138-177321485adf.gif?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "Complimentary Gifts On International Favourites",
+    discount: " Upto 30% Off +",
   },
 ];
 const cardImages4 = [
   {
     url: "https://images-static.nykaa.com/uploads/1ca5ddd3-c263-40b7-9713-d8dede28b4be.jpg?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
-    },
+    tittle: "explore more+",
+    discount: "Additional 5% Off On Rs. 399",
+  },
 ];
 const cardImages5 = [
   {
     url: "https://images-static.nykaa.com/uploads/5c11bb71-1500-477e-9943-491d57a1ec57.gif?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "explore more+",
+    discount: " Grab SUGAR Favorites On Purchase Of Rs.799",
   },
   {
     url: "https://images-static.nykaa.com/uploads/62eb4f26-ccb1-4a18-82e0-28b99368314b.jpg?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "Additional 5% Off On Rs. 399",
+    discount: "  Upto 30% Off",
   },
 ];
 const cardImages6 = [
   {
     url: "https://images-static.nykaa.com/uploads/3671a728-2665-44e1-8065-f7f75308c3cb.gif?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "explore more+",
+    discount: "50%-60%",
   },
   {
     url: "https://images-static.nykaa.com/uploads/0767a3b8-61ec-43b0-901b-76ae5563fdb8.png?tr=w-1200,cm-pad_resize",
-    tittle:"explore more+",
-    discount:"50%-60%"
+    tittle: "explore more+",
+    discount: "50%-60%",
   },
 ];
 const cardImages7 = [
@@ -210,7 +207,7 @@ function HomePage() {
                   alt="card"
                 />
                 <div className={styles.setdiv}>
-                <span className={styles.tittle}>{`${cards.tittle}`} </span>
+                  <span className={styles.tittle}>{`${cards.tittle}`} </span>
                   <p className={styles.discount}>{`${cards.discount}`}</p>
                 </div>
               </div>
@@ -237,7 +234,7 @@ function HomePage() {
                   alt="card"
                 />
                 <div className={styles.setdiv}>
-                <span className={styles.tittle}>{`${cards.tittle}`} </span>
+                  <span className={styles.tittle}>{`${cards.tittle}`} </span>
                   <p className={styles.discount}>{`${cards.discount}`}</p>
                 </div>
               </div>
@@ -307,7 +304,11 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Upto 20% Off + Free Face Scrub
+              </Text>
+              className={styles.title}
             </Stack>
           </Box>
         </Box>
@@ -333,7 +334,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Moisturizer Worth Rs. 399 On Rs. 1000
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -359,7 +363,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Free Fab Bullet Lipstick Free On ₹799
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -384,7 +391,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Flat 10% Off + Buy 5 Get 5 On Sheet Masks!
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -409,7 +419,9 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                Upto 20% Off + Free Face Wash
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -434,7 +446,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Upto 30% Off + Buy 2, Choose 2 Free Minis
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -459,7 +474,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Upto 30% Off + Buy 2, Choose 2 Free Minis
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -484,7 +502,7 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}> nykaa-naturals-all</Text>
             </Stack>
           </Box>
         </Box>
@@ -510,7 +528,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Experience Beauty From Within
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -536,7 +557,7 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}> Worth Rs. 249 On Rs. 800</Text>
             </Stack>
           </Box>
         </Box>
@@ -561,7 +582,7 @@ function HomePage() {
                   alt="card"
                 />
                 <div className={styles.setdiv}>
-                <span className={styles.tittle}>{`${cards.tittle}`}</span>
+                  <span className={styles.tittle}>{`${cards.tittle}`}</span>
                   <p className={styles.discount}>{`${cards.discount}`}</p>
                 </div>
               </div>
@@ -600,7 +621,7 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}> Up To 25% Off</Text>
             </Stack>
           </Box>
         </Box>
@@ -626,7 +647,7 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}> On Salon Quality Hair Care</Text>
             </Stack>
           </Box>
         </Box>
@@ -652,7 +673,7 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>Upto 50% Off</Text>
             </Stack>
           </Box>
         </Box>
@@ -677,7 +698,7 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>Worth Rs. 139 On Rs. 700</Text>
             </Stack>
           </Box>
         </Box>
@@ -702,7 +723,7 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>Worth Rs. 139 On Rs. 700</Text>
             </Stack>
           </Box>
         </Box>
@@ -727,7 +748,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Experience Beauty From Within
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -751,7 +775,9 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                Experience Beauty From Within
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -775,7 +801,10 @@ function HomePage() {
           />
           <Box p={6}>
             <Stack spacing={0} align={"center"} mb={5}>
-              <Text> Flat 10% Off + Buy 5 Get 5 On Sheet Masks!</Text>
+              <Text className={styles.tittle}>
+                {" "}
+                Moisturizer Worth Rs. 399 On Rs. 1000
+              </Text>
             </Stack>
           </Box>
         </Box>
@@ -799,7 +828,7 @@ function HomePage() {
                   alt="card"
                 />
                 <div className={styles.setdiv}>
-                <span className={styles.tittle}>{`${cards.tittle}`}</span>
+                  <span className={styles.tittle}>{`${cards.tittle}`}</span>
                   <p className={styles.discount}>{`${cards.discount}`}</p>
                 </div>
               </div>
@@ -840,7 +869,7 @@ function HomePage() {
                   alt="card"
                 />
                 <div className={styles.setdiv}>
-                <span className={styles.tittle}>{`${cards.tittle}`}</span>
+                  <span className={styles.tittle}>{`${cards.tittle}`}</span>
                   <p className={styles.discount}>{`${cards.discount}`}</p>
                 </div>
               </div>
--- frontend/src/components/Home/Horizontal.js
+++ frontend/src/components/Home/Horizontal.js
@@ -1,56 +1,154 @@
 import styles from "./Horizontal.module.css";
+const products = [
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    price: "899",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    price: "899",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "SUGAR Smudge Me Not Liquid Mini Lipstick Bold Set(1.1ml E...",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    price: "899",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    offprice: "₹999",
+    price: "899",
+    button: "view",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    price: "899",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    price: "899",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    price: "899",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    price: "899",
+  },
+  {
+    url: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/a/aadb28e8904320705275_1.jpg",
+    tittle: "this is a para",
+    subtittle: "1.1each",
+    badge: "BESTSELLER",
+    prize: "899",
+  },
+];
+
 function Hori() {
   return (
     <>
       <div className={styles.media_scroller}>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1641353989082-9b15fa661805?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODcyOA&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1642190672487-22bde32965f7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODcyOA&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1641841344411-49dbd02896f4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODcyOA&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1643223723262-7ce785730cf6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODcyOA&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1640938776314-4d303f8a1380?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc2Mw&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1641259041823-e09935369105?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc2Mw&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1642543492481-44e81e3914a7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc2Mw&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
-        <div className={styles.media_element}>
-          <img
-            src="https://images.unsplash.com/photo-1642543492481-44e81e3914a7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzM5ODc2Mw&ixlib=rb-1.2.1&q=80&w=400"
-            alt=""
-          />
-        </div>
+        {products.map((product, index) => (
+          <div className={styles.media_element}>
+            <img src={`${product.url}`} alt="" />
+            <p className={styles.detail}>{`${product.tittle}`}</p>
+            <div className={styles.subtittle}>1.1ml Each </div>
+            <div className="css-lf8wwq">
+              <ul className="css-op2i1d">
+                <li
+                  tabindex="0"
+                  role="button"
+                  className="bestseller default-tag css-nr4f1"
+                >
+                  BESTSELLER
+                </li>
+              </ul>
+            </div>
+            <p>{`${product.badge}`}</p>
+            <div className={styles.reviewstar}>
+              <span className={styles.starwrap}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="7">
+                  <path
+                    fill="#2E2F3A"
+                    d="M1.536 7c-.03 0-.06-.015-.09-.03a.162.162 0 01-.06-.161l.66-2.412-1.98-1.573c-.06-.03-.076-.103-.06-.162a.144.144 0 01.134-.103l2.552-.133.915-2.338c.03-.044.09-.088.15-.088s.12.044.135.088l.916 2.338 2.551.133c.06 0 .12.044.135.103a.164.164 0 01-.045.162L5.47 4.397l.66 2.412a.145.145 0 01-.06.162c-.045.029-.12.044-.165 0L3.757 5.618 1.611 6.97c-.03.03-.045.03-.075.03z"
+                  ></path>
+                </svg>
+              </span>
+              <span className={styles.starwrap}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="7">
+                  <path
+                    fill="#2E2F3A"
+                    d="M1.536 7c-.03 0-.06-.015-.09-.03a.162.162 0 01-.06-.161l.66-2.412-1.98-1.573c-.06-.03-.076-.103-.06-.162a.144.144 0 01.134-.103l2.552-.133.915-2.338c.03-.044.09-.088.15-.088s.12.044.135.088l.916 2.338 2.551.133c.06 0 .12.044.135.103a.164.164 0 01-.045.162L5.47 4.397l.66 2.412a.145.145 0 01-.06.162c-.045.029-.12.044-.165 0L3.757 5.618 1.611 6.97c-.03.03-.045.03-.075.03z"
+                  ></path>
+                </svg>
+              </span>
+              <span className={styles.starwrap}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="7">
+                  <path
+                    fill="#2E2F3A"
+                    d="M1.536 7c-.03 0-.06-.015-.09-.03a.162.162 0 01-.06-.161l.66-2.412-1.98-1.573c-.06-.03-.076-.103-.06-.162a.144.144 0 01.134-.103l2.552-.133.915-2.338c.03-.044.09-.088.15-.088s.12.044.135.088l.916 2.338 2.551.133c.06 0 .12.044.135.103a.164.164 0 01-.045.162L5.47 4.397l.66 2.412a.145.145 0 01-.06.162c-.045.029-.12.044-.165 0L3.757 5.618 1.611 6.97c-.03.03-.045.03-.075.03z"
+                  ></path>
+                </svg>
+              </span>
+              <span className={styles.starwrap}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="7">
+                  <path
+                    fill="#2E2F3A"
+                    d="M1.536 7c-.03 0-.06-.015-.09-.03a.162.162 0 01-.06-.161l.66-2.412-1.98-1.573c-.06-.03-.076-.103-.06-.162a.144.144 0 01.134-.103l2.552-.133.915-2.338c.03-.044.09-.088.15-.088s.12.044.135.088l.916 2.338 2.551.133c.06 0 .12.044.135.103a.164.164 0 01-.045.162L5.47 4.397l.66 2.412a.145.145 0 01-.06.162c-.045.029-.12.044-.165 0L3.757 5.618 1.611 6.97c-.03.03-.045.03-.075.03z"
+                  ></path>
+                </svg>
+              </span>
+              <span className={styles.starwrap}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="8"
+                  height="7"
+                  viewBox="0 0 15 14"
+                >
+                  <path
+                    fill="#2E2F3A"
+                    d="M7.5 0a.3.3 0 00-.28.188L5.37 4.863l-5.085.255a.3.3 0 00-.27.202.29.29 0 00.095.32l3.96 3.174-1.3 4.816a.291.291 0 00.113.314c.1.071.235.075.34.01L7.5 11.26l4.278 2.692a.305.305 0 00.339-.01.291.291 0 00.113-.313l-1.3-4.816 3.96-3.175a.29.29 0 00.095-.32.3.3 0 00-.27-.201L9.63 4.863 7.78.188A.3.3 0 007.5 0zm0 1l1.664 4.166a.304.304 0 00.268.188L14 5.581l-3.558 2.826a.29.29 0 00-.102.304L11.51 13l-3.845-2.397a.312.312 0 00-.33 0L3.49 13l1.17-4.29a.29.29 0 00-.102-.303L1 5.58l4.568-.227a.304.304 0 00.268-.188L7.5 1z"
+                  ></path>
+                </svg>
+              </span>
+              <span className={styles.reviewcount}>1776</span>
+            </div>
+            <div className={styles.price1}>
+              <span className={styles.offprice}>
+                <span>{`${product.price}`}</span>
+              </span>
+              <span
+                className={styles.pricediscountprice}
+              >{`${product.price}`}</span>
+              <span className={styles.priceoffer}>{`${product.price}`}</span>
+            </div>
+            <p>{`${product.subtittle}`}</p>
+            <button className={styles.viewbtn}>view</button>
+          </div>
+        ))}
       </div>
     </>
   );
