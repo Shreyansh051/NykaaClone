7924ca7627184e732b7b74b7ccef7ae4a5c93842
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-23 16:58:43 +0530

    updated controller

--- backend/Controllers/cartController.js
+++ backend/Controllers/cartController.js
@@ -1,61 +1,76 @@
 const cartItem = require("../Models/cartModel");
+const { findByIdAndUpdate } = require("../Models/ProductModel");
 const products = require("../Models/ProductModel");
 
-
-module.exports.addToBag = async (req,res) => {
-    try {
-        const {id} = req.params;
-        const {image_url, tags, name, price, s_price, discount, rating, n_ratings,n_reviews} = await products.findById(id);
-        await cartItem.create({
-         image_url,
-         tags,
-         name,
-         price,
-         s_price,
-         discount,
-         rating,
-         n_ratings,
-         n_reviews
-        });
-        const AllCarts = await cartItem.find();
-        res.status(200).json(AllCarts);
-        
-    } catch (error) {
-        console.log(error.message);
+module.exports.addToBag = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      image_url,
+      tags,
+      name,
+      price,
+      s_price,
+      discount,
+      rating,
+      n_ratings,
+      n_reviews,
+    } = await products.findById(id);
+    const s_cartItem = await cartItem.findOne({name: name});
+    if (s_cartItem) {
+      await cartItem.findByIdAndUpdate(s_cartItem._id, {
+        $inc: { quantity: 1 },
+      });
+    } else {
+      await cartItem.create({
+        image_url,
+        tags,
+        name,
+        price,
+        s_price,
+        discount,
+        rating,
+        n_ratings,
+        n_reviews,
+      });
     }
-  
-}
+    const AllCarts = await cartItem.find();
+    res.status(200).json(AllCarts);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports.getAllCart = async (req,res) => {
-    try {
-        const getallcart = await cartItem.find();
-        res.status(200).json(getallcart);
-    } catch (error) {
-        res.send(error.message);
-    }
-}
+module.exports.getAllCart = async (req, res) => {
+  try {
+    const getallcart = await cartItem.find();
+    res.status(200).json(getallcart);
+  } catch (error) {
+    res.send(error.message);
+  }
+};
 
-module.exports.setQuantity = async (req,res) => {
-    try {
-        const {quant} = req.body;
-        const {id} = req.params;
-       const cartitem =  await cartItem.findByIdAndUpdate(id,{
-            quantity: quant
-        })
-        const cartArray = await cartItem.find();
-        res.json(cartArray);
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+module.exports.setQuantity = async (req, res) => {
+  try {
+    const { quant } = req.body;
+    const { id } = req.params;
+    const cartitem = await cartItem.findByIdAndUpdate(id, {
+      quantity: quant,
+    });
+    const cartArray = await cartItem.find();
+    res.json(cartArray);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports.deleteItem = async (req,res) => {
-    try {
-        const {id} = req.params;
-        await cartItem.findByIdAndDelete(id);
-        const cartArray = await cartItem.find();
-        res.json(cartArray);
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+module.exports.deleteItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await cartItem.findByIdAndDelete(id);
+    const cartArray = await cartItem.find();
+    res.json(cartArray);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
