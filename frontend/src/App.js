65160ed5160c84ee3090d17a3c11c605fe393df5
SatsAjeet <98746450+SatsAjeet@users.noreply.github.com>
2022-07-23 17:59:27 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,17 @@ import './App.css';
 // import Footer from './components/Footer/Footer';
 // import MainNavbar from './components/Navbar/MainNavbar';
 import Products from './components/Product page/Products';
-
-
-
 import HomePage from "./components/Home/HomePage"
 // import Products from "./components/Product page/Products";
-// import Main from './Main';
+import Main from './Main';
 // import Products from './components/Product page/Products';
 // import { Siderbar } from './components/Product page/Sidebar';
 function App() {
   return (
     <div className="App">
     {/* <Products/> */}
-    <HomePage/>
+      {/* <HomePage/> */}
+      <Main/>
     {/* <Siderbar/> */}
     </div>
   )};
